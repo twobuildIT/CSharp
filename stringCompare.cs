b8a65da712c802e1b6a5ae4308bbb39db1a4f99e
twobuildIT <jatjer@gmail.com>
2016-11-24 06:37:07 -0800

    StringCompare

---
+++ stringCompare.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class stringCompare
+{
+	public stringCompare()
+	{
+	}
+
+        private string oneStr, twoStr;
+
+
+    private string getString()
+    {
+        string userString;
+        Console.WriteLine("Please enter text");
+        userString = Convert.ToString(Console.ReadLine());
+        return userString;
+
+    }
+
+    private void setStrings()
+    {
+        oneStr = getString();
+        twoStr = getString();
+
+    }
+
+    public void strCompare()
+    {
+        if (String.Compare(oneStr, twoStr) == 0)
+        {
+            Console.WriteLine("Values entered are the same");
+        }
+        else
+        {
+            Console.WriteLine("String values are not the same");
+        }
+    }
+
+    public void strExecute()
+    {
+        setStrings();
+        strCompare();
+    }
+
+
+}
+
+
+
+
+
+
+
