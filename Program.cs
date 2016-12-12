e5bce99c0e40a90873e3c9e34be35f69afa8c50a
Jermaine Thompson <jatjer@gmail.com>
2016-12-12 03:27:07 -0800

    Factorial Program

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        private int fact(int n)
+        {
+            int nf = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                nf *= i;
+            }
+            return nf;
+        }
+
+        private void printFact(int n)
+        {
+           
+            Console.WriteLine(fact(n));
+        }
+
+        private int factorial(int x)
+        {
+            if (x != 0)
+            {
+                return x * factorial(x - 1);
+                
+            }
+            else
+            {
+                return 1;
+            }
+            
+        }
+        private void printFactorial(int num)
+        {
+            Console.WriteLine(factorial(num));
+        }
+
+        static void Main(string[] args)
+        {
+
+            //int num;
+            //Console.WriteLine("enter numer");
+            //num = Console.Read();
+            Program c = new Program();
+            c.printFact(9);
+            c.printFactorial(9);
+            Console.ReadKey();
+
+
+        }
+    }
+}
